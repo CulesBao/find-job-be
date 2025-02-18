afff4739e681e4fb25418747313fc0f3aeb65c45
CulesBao <102230334@sv1.dut.udn.vn>
2025-02-18 07:23:22 +0700

    feat: first commmit

---
+++ src/test/java/com/findjobbe/findjobbe/FindJobBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.findjobbe.findjobbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindJobBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
