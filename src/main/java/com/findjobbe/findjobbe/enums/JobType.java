fa460fa692f48a99605c3c21170f430f254d0630
QT-2005 <thinhquangtran2005@gmail.com>
2025-03-16 15:37:08 +0700

    Update JobType

--- src/main/java/com/findjobbe/findjobbe/enums/JobType.java
+++ src/main/java/com/findjobbe/findjobbe/enums/JobType.java
@@ -1,6 +1,9 @@
 package com.findjobbe.findjobbe.enums;
 
 public enum JobType {
+    // ---------------------
+    // LĨNH VỰC CÔNG NGHỆ THÔNG TIN
+    // ---------------------
     SOFTWARE_ENGINEER,
     FRONTEND_DEVELOPER,
     BACKEND_DEVELOPER,
@@ -9,22 +12,128 @@ public enum JobType {
     DATA_SCIENTIST,
     MACHINE_LEARNING_ENGINEER,
     DEVOPS_ENGINEER,
-    SYSTEM_ADMINISTRATOR,
-    DATABASE_ADMINISTRATOR,
-    CLOUD_ENGINEER,
-    CYBER_SECURITY_SPECIALIST,
-    QA_ENGINEER,
-    TEST_AUTOMATION_ENGINEER,
-    PRODUCT_MANAGER,
-    PROJECT_MANAGER,
+    QA_TESTER,
     UI_UX_DESIGNER,
-    SCRUM_MASTER,
-    BUSINESS_ANALYST,
-    TECHNICAL_SUPPORT_ENGINEER,
+    IT_SUPPORT,
     NETWORK_ENGINEER,
-    GAME_DEVELOPER,
-    EMBEDDED_SYSTEMS_ENGINEER,
-    BLOCKCHAIN_DEVELOPER,
-    AI_ENGINEER,
-    IT_HELPDESK
+    CYBER_SECURITY_SPECIALIST,
+
+    // ---------------------
+    // LĨNH VỰC Y TẾ - CHĂM SÓC SỨC KHỎE
+    // ---------------------
+    DOCTOR,
+    NURSE,
+    PHARMACIST,
+    DENTIST,
+    VETERINARIAN,
+    THERAPIST,
+    PSYCHOLOGIST,
+    NUTRITIONIST,
+
+    // ---------------------
+    // LĨNH VỰC GIÁO DỤC
+    // ---------------------
+    TEACHER,
+    LECTURER,
+    TUTOR,
+    SCHOOL_COUNSELOR,
+    EDUCATION_COORDINATOR,
+
+    // ---------------------
+    // LĨNH VỰC TÀI CHÍNH - KẾ TOÁN
+    // ---------------------
+    ACCOUNTANT,
+    FINANCIAL_ANALYST,
+    BANK_TELLER,
+    INVESTMENT_BANKER,
+    FINANCIAL_ADVISOR,
+    CREDIT_ANALYST,
+    TAX_SPECIALIST,
+
+    // ---------------------
+    // LĨNH VỰC KINH DOANH - QUẢN TRỊ
+    // ---------------------
+    BUSINESS_ANALYST,
+    PROJECT_MANAGER,
+    PRODUCT_MANAGER,
+    HR_MANAGER,
+    RECRUITER,
+    OPERATIONS_MANAGER,
+    OFFICE_ADMINISTRATOR,
+
+    // ---------------------
+    // MARKETING & BÁN HÀNG
+    // ---------------------
+    MARKETING_SPECIALIST,
+    SOCIAL_MEDIA_MANAGER,
+    SEO_SPECIALIST,
+    SALES_ASSOCIATE,
+    SALES_MANAGER,
+    BRAND_MANAGER,
+    DIGITAL_MARKETING_EXECUTIVE,
+
+    // ---------------------
+    // DỊCH VỤ KHÁCH HÀNG - HÀNH CHÍNH
+    // ---------------------
+    CUSTOMER_SERVICE_REPRESENTATIVE,
+    RECEPTIONIST,
+    CALL_CENTER_AGENT,
+
+    // ---------------------
+    // NHÀ HÀNG - KHÁCH SẠN - DU LỊCH
+    // ---------------------
+    HOTEL_MANAGER,
+    RESTAURANT_MANAGER,
+    CHEF,
+    WAITER,
+    TOUR_GUIDE,
+    EVENT_PLANNER,
+
+    // ---------------------
+    // LĨNH VỰC XÂY DỰNG - KỸ THUẬT
+    // ---------------------
+    CIVIL_ENGINEER,
+    ELECTRICAL_ENGINEER,
+    MECHANICAL_ENGINEER,
+    ARCHITECT,
+    SURVEYOR,
+    CONSTRUCTION_MANAGER,
+
+    // ---------------------
+    // LĨNH VỰC PHÁP LUẬT
+    // ---------------------
+    LAWYER,
+    PARALEGAL,
+    LEGAL_ASSISTANT,
+    LEGAL_SECRETARY,
+
+    // ---------------------
+    // LĨNH VỰC SÁNG TẠO - TRUYỀN THÔNG
+    // ---------------------
+    GRAPHIC_DESIGNER,
+    COPYWRITER,
+    VIDEO_EDITOR,
+    PHOTOGRAPHER,
+    CONTENT_CREATOR,
+
+    // ---------------------
+    // VẬN CHUYỂN - LOGISTICS
+    // ---------------------
+    DRIVER,
+    DELIVERY_PERSON,
+    SUPPLY_CHAIN_MANAGER,
+    WAREHOUSE_COORDINATOR,
+
+    // ---------------------
+    // CÔNG VIỆC KHÁC (CHÍNH PHỦ, NGHIÊN CỨU, V.V.)
+    // ---------------------
+    POLICE_OFFICER,
+    FIREFIGHTER,
+    SCIENTIST,
+    LIBRARIAN,
+
+    // ---------------------
+    // LỰA CHỌN KHÁC (KHÔNG THUỘC NHÓM NÀO Ở TRÊN)
+    // ---------------------
+    OTHER
 }
